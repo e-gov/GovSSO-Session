e6b32baeb83204208da93add770c635784b9ae27
Juku Trump <juku.trump@nortal.com>
2022-01-26 09:58:04 +0200

    GSSO-214 Build deployable WAR for govsso-session

--- src/main/java/ee/ria/govsso/session/Application.java
+++ src/main/java/ee/ria/govsso/session/Application.java
@@ -2,9 +2,17 @@ package ee.ria.govsso.session;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    // Support servlet container as described in https://docs.spring.io/spring-boot/docs/2.6.3/reference/htmlsingle/#howto.traditional-deployment.war
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
