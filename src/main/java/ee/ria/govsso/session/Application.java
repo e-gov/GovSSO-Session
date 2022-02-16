837f6e72623a39b593af3c00eb2164e47dcd045c
Alar Kvell <alar.kvell@nortal.com>
2022-02-16 19:32:35 +0200

    GSSO-270 Disable autoconfiguration of UserDetailsService

--- src/main/java/ee/ria/govsso/session/Application.java
+++ src/main/java/ee/ria/govsso/session/Application.java
@@ -2,10 +2,11 @@ package ee.ria.govsso.session;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
