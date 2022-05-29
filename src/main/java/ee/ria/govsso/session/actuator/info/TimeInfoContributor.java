d7e450eb2e145b95928e44792b4e1221f145531d
Jorgen Heinsoo <jorgen.heinsoo@nortal.com>
2022-05-29 22:55:07 +0300

    GSSO-438 Add server current time and service start time to actuator info endpoint

---
+++ src/main/java/ee/ria/govsso/session/actuator/info/TimeInfoContributor.java
@@ -0,0 +1,38 @@
+package ee.ria.govsso.session.actuator.info;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.TimeGauge;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+class TimeInfoContributor implements InfoContributor {
+
+    private final MeterRegistry meterRegistry;
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder
+                .withDetail("startTime", getServiceStartTime())
+                .withDetail("currentTime", OffsetDateTime.now(ZoneOffset.UTC));
+    }
+
+    private OffsetDateTime getServiceStartTime() {
+        TimeGauge startTime = meterRegistry.find("process.start.time").timeGauge();
+        if (startTime == null) {
+            throw new IllegalStateException("Failed to get application start time");
+        }
+        long startTimeEpochMilli = Double.valueOf(startTime.value(TimeUnit.MILLISECONDS)).longValue();
+        Instant startTimeInstant = Instant.ofEpochMilli(startTimeEpochMilli);
+        return OffsetDateTime.ofInstant(startTimeInstant, ZoneId.of("UTC"));
+    }
+}
---
+++ src/test/java/ee/ria/govsso/session/actuator/info/InfoEndpointTest.java
@@ -0,0 +1,51 @@
+package ee.ria.govsso.session.actuator.info;
+
+import ee.ria.govsso.session.BaseTest;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class InfoEndpointTest extends BaseTest {
+
+    @Test
+    void info_currentTimeIsPresent() {
+        String serveCurrentTimeStr = given()
+                .when()
+                .get("/actuator/info")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .path("currentTime");
+
+        assertNotNull(serveCurrentTimeStr);
+        OffsetDateTime now = OffsetDateTime.now(ZoneOffset.UTC);
+        OffsetDateTime serveCurrentTime = OffsetDateTime.parse(serveCurrentTimeStr);
+        assertTrue(serveCurrentTime.isBefore(now));
+        assertTrue(serveCurrentTime.isAfter(now.minus(Duration.ofMinutes(1))));
+    }
+
+    @Test
+    void info_startTimeIsPresent() {
+        String serviceStartTimeStr = given()
+                .when()
+                .get("/actuator/info")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .path("startTime");
+
+        assertNotNull(serviceStartTimeStr);
+        OffsetDateTime now = OffsetDateTime.now(ZoneOffset.UTC);
+        OffsetDateTime serveCurrentTime = OffsetDateTime.parse(serviceStartTimeStr);
+        assertTrue(serveCurrentTime.isBefore(now));
+        assertTrue(serveCurrentTime.isAfter(now.minus(Duration.ofMinutes(1))));
+    }
+}
