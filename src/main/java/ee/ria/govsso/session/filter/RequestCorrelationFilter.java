0042e58afd79ee34b330789fcd58fda14437ce16
Alar Kvell <alar.kvell@nortal.com>
2022-06-17 14:35:39 +0300

    GSSO-119 Ensure all MDC attributes are reset at the start of request

--- src/main/java/ee/ria/govsso/session/filter/RequestCorrelationFilter.java
+++ src/main/java/ee/ria/govsso/session/filter/RequestCorrelationFilter.java
@@ -12,7 +12,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Locale;
 
 @RequiredArgsConstructor
 public class RequestCorrelationFilter extends OncePerRequestFilter {
@@ -30,7 +29,8 @@ public class RequestCorrelationFilter extends OncePerRequestFilter {
 
         String requestTraceId = MDC.get(MDC_ATTRIBUTE_KEY_REQUEST_TRACE_ID);
         if (StringUtils.isEmpty(requestTraceId)) {
-            MDC.put(MDC_ATTRIBUTE_KEY_REQUEST_TRACE_ID, RandomStringUtils.random(32, "0123456789abcdef").toLowerCase(Locale.ROOT));
+            requestTraceId = RandomStringUtils.random(32, "0123456789abcdef");
+            MDC.put(MDC_ATTRIBUTE_KEY_REQUEST_TRACE_ID, requestTraceId);
         }
 
         // NB! Set traceId also as HttpServletRequest attribute to make it accessible for Tomcat's AccessLogValve
@@ -43,8 +43,16 @@ public class RequestCorrelationFilter extends OncePerRequestFilter {
         String ipAddress = request.getRemoteAddr();
         if (StringUtils.isNotEmpty(ipAddress)) {
             MDC.put(MDC_ATTRIBUTE_KEY_CLIENT_IP, ipAddress);
+        } else {
+            MDC.remove(MDC_ATTRIBUTE_KEY_CLIENT_IP);
         }
 
         filterChain.doFilter(request, response);
+
+        // TODO Ideally all MDC values that are set here should be cleared after request completes - investigate where
+        //  is the correct place to do that. Doing it here clears trace.id too early so that error page can not access
+        //  it.
+        //  As the second-best solution, we are (re)setting _all_ values above at the start of request. Usually request
+        //  threads and background threads are not cross-used.
     }
 }
