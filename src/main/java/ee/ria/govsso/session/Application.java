0858cf0037b0a20f30e31b0868342b84c19d3396
Jaagup Kümmel <jaagup.kymmel@nortal.com>
2024-06-13 09:43:41 +0300

    AUT-1546 Use JAR packaging

--- src/main/java/ee/ria/govsso/session/Application.java
+++ src/main/java/ee/ria/govsso/session/Application.java
@@ -4,21 +4,12 @@ import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         ElasticApmAttacher.attach();
         SpringApplication.run(Application.class, args);
-    }
-
-    // Support servlet container as described in https://docs.spring.io/spring-boot/docs/2.7.x/reference/htmlsingle/#howto.traditional-deployment.war
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        ElasticApmAttacher.attach();
-        return application.sources(Application.class);
     }
 }
