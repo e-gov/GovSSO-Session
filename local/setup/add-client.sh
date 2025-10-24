4da0dfebb13c028ae4edab3e9115979a613d4c14
Jaagup Kümmel <jaagup.kymmel@nortal.com>
2025-10-24 13:51:46 +0300

    AUT-2304 Update client setup script to set client secret

--- local/setup/add-client.sh
+++ local/setup/add-client.sh
@@ -4,88 +4,121 @@ set -u
 adminServiceUrl=$1
 adminServiceUsername=$2
 adminServicePassword=$3
-institution=$4
-createInstitutionPayload=$5
-clientId=$6
-createClientPayload=$7
+oidcServiceUrl=$4
+institution=$5
+createInstitutionPayload=$6
+clientId=$7
+createClientPayload=$8
+clientSecret=$9
 # curl resolves *.localhost addresses to 127.0.0.1, work around it with curl --resolve option.
-ipaddress=$(getent hosts $(echo ${adminServiceUrl} | sed -e 's/:.*//') | awk '{ print $1 }')
+adminServiceIp=$(getent hosts $(echo ${adminServiceUrl} | sed -e 's/:.*//') | awk '{ print $1 }')
+oidcServiceIp=$(getent hosts $(echo ${oidcServiceUrl} | sed -e 's/:.*//') | awk '{ print $1 }')
 
-echo
-echo "----- [ Load home page for XSRF token ]"
-echo
+banner() {
+    text="$*"
+    len=$(echo -n "$text" | wc -m)
+    line=$(printf '─%.0s' $(seq 1 $len))
+    echo "┌─$line─┐"
+    echo "│ $text │"
+    echo "└─$line─┘"
+}
+
+banner "Load home page for XSRF token"
 curl --insecure \
   --request GET \
   --cookie-jar cookies.txt \
-  --resolve "$adminServiceUrl:${ipaddress}" \
+  --resolve "$adminServiceUrl:${adminServiceIp}" \
   --url "https://$adminServiceUrl/" \
   --header "Content-Type: application/json" \
 
 XSRFTOKEN=$(grep -oP '__Host-XSRF-TOKEN\s*\K([\w-]+)' cookies.txt)
 
-echo
-echo "----- [ Login to admin service ]"
-echo
+banner "Login to admin service"
 #TODO: possible to use --cacert to pass truststore instead of --insecure
 curl --insecure \
   --request POST \
   --cookie cookies.txt \
   --cookie-jar cookies.txt \
-  --resolve "$adminServiceUrl:${ipaddress}" \
+  --resolve "$adminServiceUrl:${adminServiceIp}" \
   --url "https://$adminServiceUrl/login" \
   --header "Content-Type: application/json" \
   --header "X-XSRF-TOKEN: $XSRFTOKEN" \
   --data "{\"username\":\"$adminServiceUsername\",\"password\":\"$adminServicePassword\"}"
+echo
 
-echo
-echo "----- [ Delete client: $clientId ]"
-echo
-http_response=$(curl --silent --output response.txt --write-out "%{http_code}" --insecure --request DELETE --cookie cookies.txt --header "X-XSRF-TOKEN: $XSRFTOKEN" --retry-connrefused --retry-delay 15 --resolve "$adminServiceUrl:${ipaddress}" https://$adminServiceUrl/institutions/$institution/clients/$clientId)
+banner "Delete client: $clientId"
+deleteClientUrl="https://$adminServiceUrl/institutions/$institution/clients/$clientId"
+echo "DELETE $deleteClientUrl"
+http_response=$(curl --silent --output response.txt --write-out "%{http_code}" --insecure --request DELETE --cookie cookies.txt --header "X-XSRF-TOKEN: $XSRFTOKEN" --retry-connrefused --retry-delay 15 --resolve "$adminServiceUrl:${adminServiceIp}" $deleteClientUrl)
 
 echo "response code = '$http_response'"
 
 if [ "$http_response" = 200 ]; then
         echo "Existing client successfully removed"
-        echo
 else
         echo "Error when removing existing client: $clientId"
         echo "Response: $(cat response.txt)"
 fi
 
-echo
-echo "----- [ Delete institution: $institution ]"
-echo
-http_response=$(curl --silent --output response.txt --write-out "%{http_code}" --insecure --request DELETE --cookie cookies.txt --header "X-XSRF-TOKEN: $XSRFTOKEN" --retry-connrefused --retry-delay 15 --resolve "$adminServiceUrl:${ipaddress}" https://$adminServiceUrl/institutions/$institution)
+banner "Delete institution: $institution"
+deleteInstitutionUrl="https://$adminServiceUrl/institutions/$institution"
+echo "DELETE $deleteInstitutionUrl"
+http_response=$(curl --silent --output response.txt --write-out "%{http_code}" --insecure --request DELETE --cookie cookies.txt --header "X-XSRF-TOKEN: $XSRFTOKEN" --retry-connrefused --retry-delay 15 --resolve "$adminServiceUrl:${adminServiceIp}" $deleteInstitutionUrl)
 
 echo "response code = '$http_response'"
 
 if [ "$http_response" = 200 ]; then
         echo "Existing institution successfully removed"
-        echo
 else
         echo "Error when removing existing institution: $institution"
         echo "Response: $(cat response.txt)"
 fi
 
-echo "----- [ Create institution: $institution from file: $createInstitutionPayload ]"
-http_response=$(curl --silent --output response.txt --write-out "%{http_code}" --insecure --request POST --cookie cookies.txt --header "X-XSRF-TOKEN: $XSRFTOKEN" --retry-connrefused --retry-delay 15 --resolve "$adminServiceUrl:${ipaddress}" https://$adminServiceUrl/institutions -H 'Content-Type: application/json' --data-binary "@$createInstitutionPayload")
+banner "Create institution: $institution from file: $createInstitutionPayload"
+createInstitutionUrl="https://$adminServiceUrl/institutions"
+echo "POST $createInstitutionUrl"
+http_response=$(curl --silent --output response.txt --write-out "%{http_code}" --insecure --request POST --cookie cookies.txt --header "X-XSRF-TOKEN: $XSRFTOKEN" --retry-connrefused --retry-delay 15 --resolve "$adminServiceUrl:${adminServiceIp}" $createInstitutionUrl -H 'Content-Type: application/json' --data-binary "@$createInstitutionPayload")
 
 if [ "$http_response" = 200 ]; then
        echo "Institution successfully added"
-       echo
 else
        echo "Error when adding new institution: $institution"
        echo "Response: $(cat response.txt)"
 fi
 
-echo "----- [ Create client: $clientId from file: $createClientPayload ]"
-http_response=$(curl --silent --output response.txt --write-out "%{http_code}" --insecure --request POST --cookie cookies.txt --header "X-XSRF-TOKEN: $XSRFTOKEN" --retry-connrefused --retry-delay 15 --resolve "$adminServiceUrl:${ipaddress}" https://$adminServiceUrl/institutions/$institution/clients -H 'Content-Type: application/json' --data-binary "@$createClientPayload")
+banner "Create client: $clientId from file: $createClientPayload"
+createClientUrl="https://$adminServiceUrl/institutions/$institution/clients"
+echo "POST $createClientUrl"
+http_response=$(curl --silent --output response.txt --write-out "%{http_code}" --insecure --request POST --cookie cookies.txt --header "X-XSRF-TOKEN: $XSRFTOKEN" --retry-connrefused --retry-delay 15 --resolve "$adminServiceUrl:${adminServiceIp}" $createClientUrl -H 'Content-Type: application/json' --data-binary "@$createClientPayload")
 
 if [ "$http_response" = 200 ]; then
        echo "Client successfully added"
-       echo
 else
        echo "Unexpected error when adding new client: $clientId"
+       echo "Response: $(cat response.txt)"
+       exit 1
+fi
+
+banner "Set client: $clientId secret"
+setClientSecretPayload=$(cat <<EOF
+[
+  {
+    "op": "replace",
+    "path": "/client_secret",
+    "value": "$clientSecret"
+  }
+]
+EOF
+)
+
+setClientSecretUrl="https://$oidcServiceUrl/admin/clients/$clientId"
+echo "PATCH $setClientSecretUrl"
+http_response=$(curl --silent --output response.txt --write-out "%{http_code}" --insecure --request PATCH --retry-connrefused --retry-delay 15  --resolve "$oidcServiceUrl:${oidcServiceIp}" $setClientSecretUrl -H 'Content-Type: application/json' --data "$setClientSecretPayload")
+
+if [ "$http_response" = 200 ]; then
+       echo "Client secret successfully set"
+else
+       echo "Unexpected error setting client secret"
        echo "Response: $(cat response.txt)"
        exit 1
 fi
--- local/setup/init.sh
+++ local/setup/init.sh
@@ -5,28 +5,28 @@ echo 'Check - is admin service API to be ready to accept requests...'
 ./wait-for.sh $ADMIN_SERVICE_URL --timeout=300 -- echo "the $ADMIN_SERVICE_URL is up" || { echo "the $ADMIN_SERVICE_URL is not available"; exit 1; }
 
 echo 'Adding client A'
-./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS '70000001' './institution-a.json' 'client-a' './client-a.json'
+./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS $OIDC_SERVICE_URL '70000001' './institution-a.json' 'client-a' './client-a.json' 'secreta'
 
 echo 'Adding client B'
-./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS '70000002' './institution-b.json' 'client-b' './client-b.json'
+./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS $OIDC_SERVICE_URL '70000002' './institution-b.json' 'client-b' './client-b.json' 'secretb'
 
 echo 'Adding client C'
-./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS '70000001' './institution-a.json' 'client-c' './client-c.json'
+./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS $OIDC_SERVICE_URL '70000001' './institution-a.json' 'client-c' './client-c.json' 'secretc'
 
 echo 'Adding client D'
-./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS '70000002' './institution-b.json' 'client-d' './client-d.json'
+./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS $OIDC_SERVICE_URL '70000002' './institution-b.json' 'client-d' './client-d.json' 'secretd'
 
 echo 'Adding client E'
-./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS '70000001' './institution-a.json' 'client-e' './client-e.json'
+./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS $OIDC_SERVICE_URL '70000001' './institution-a.json' 'client-e' './client-e.json' 'secrete'
 
 echo 'Adding client F'
-./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS '70000002' './institution-b.json' 'client-f' './client-f.json'
+./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS $OIDC_SERVICE_URL '70000002' './institution-b.json' 'client-f' './client-f.json' 'secretf'
 
 echo 'Adding client Mock ACR Low'
-./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS '70000003' './institution-mock.json' 'client-mock-acr-low' './client-mock-acr-low.json'
+./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS $OIDC_SERVICE_URL '70000003' './institution-mock.json' 'client-mock-acr-low' './client-mock-acr-low.json' 'secret'
 
 echo 'Adding client Mock ACR Substantial'
-./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS '70000003' './institution-mock.json' 'client-mock-acr-substantial' './client-mock-acr-substantial.json'
+./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS $OIDC_SERVICE_URL '70000003' './institution-mock.json' 'client-mock-acr-substantial' './client-mock-acr-substantial.json' 'secret'
 
 echo 'Adding client Mock ACR High'
-./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS '70000003' './institution-mock.json' 'client-mock-acr-high' './client-mock-acr-high.json'
+./add-client.sh $ADMIN_SERVICE_URL $ADMIN_USER $ADMIN_PASS $OIDC_SERVICE_URL '70000003' './institution-mock.json' 'client-mock-acr-high' './client-mock-acr-high.json' 'secret'
