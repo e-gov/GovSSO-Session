0358a9e32b566271ba9cc87a0af0b295b63dffe8
Alar Kvell <alar.kvell@nortal.com>
2025-12-08 13:57:34 +0200

    Example: use TRUNCATE instead of DELETE

--- local/hydra-janitor/delete_rows.sql
+++ local/hydra-janitor/delete_rows.sql
@@ -1,31 +1,6 @@
-DELETE
-FROM public.hydra_oauth2_authentication_session
-;
+TRUNCATE public.hydra_oauth2_authentication_session, public.hydra_oauth2_code, public.hydra_oauth2_oidc, public.hydra_oauth2_pkce, public.hydra_oauth2_flow, public.hydra_oauth2_access, public.hydra_oauth2_refresh, public.hydra_oauth2_logout_request;
 
-DELETE
-FROM public.hydra_oauth2_code
-;
-
-DELETE
-FROM public.hydra_oauth2_oidc
-;
-
-DELETE
-FROM public.hydra_oauth2_pkce
-;
-
-DELETE
-FROM public.hydra_oauth2_flow
-;
-
-DELETE FROM public.hydra_oauth2_access;
-
-DELETE FROM public.hydra_oauth2_refresh;
 -- hydra_oauth2_jti_blacklist, hydra_oauth2_obfuscated_authentication_session, hydra_oauth2_trusted_jwt_bearer_issuer
 -- are empty in our use cases
-
-DELETE
-FROM public.hydra_oauth2_logout_request
-;
 
 -- hydra_client, hydra_jwk, schema_migration must NOT be cleaned
