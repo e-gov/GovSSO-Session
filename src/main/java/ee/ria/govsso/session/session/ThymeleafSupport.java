3b5181de2a0d02c270706087b83604c52a712287
Sander Kondratjev <sander.kondratjev@nortal.com>
2026-05-15 09:59:26 +0300

    AUT-2602 Replace restricted Thymeleaf locale expressions in language selector
    (cherry picked from commit ba7006e0c8cee30945b4221eb08791a86e9a3bd4)

--- src/main/java/ee/ria/govsso/session/session/ThymeleafSupport.java
+++ src/main/java/ee/ria/govsso/session/session/ThymeleafSupport.java
@@ -1,22 +1,14 @@
 package ee.ria.govsso.session.session;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponents;
-
-import java.util.Locale;
 
 @Slf4j
 @Component
 public class ThymeleafSupport {
 
-    public boolean isNotLocale(String code, Locale locale) {
-        return !locale.getLanguage().equalsIgnoreCase(code);
-    }
-
-    public String getLocaleUrl(String locale) {
-        UriComponents uriComponents = ServletUriComponentsBuilder.fromCurrentRequest().replaceQueryParam("lang", locale).build();
-        return uriComponents.getPath() + "?" + uriComponents.getQuery();
+    public boolean isNotLocale(String code) {
+        return !LocaleContextHolder.getLocale().getLanguage().equalsIgnoreCase(code);
     }
 }
